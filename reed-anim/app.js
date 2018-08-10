550358af65f4e12c8ccbc873e1ae5aae94abb234
ogadoury <32474674+ogadoury@users.noreply.github.com>
2018-08-09 22:36:39 -0400

    Added dots

--- reed-anim/app.js
+++ reed-anim/app.js
@@ -1,40 +1,10 @@
-const cont = $( '#container' );
-const letter = $( '#letter' );
-
-
-// DOT BACKGROUND
-// 		--- put function here ---
-
-
-// function createDots(x) {
-// 	var contWidth = $( '#container' ).width;
-// 	var contHeight = $ ( '#container' ).height;
-//     for (var rows = 0; rows < x; rows++) {
-//         for (var columns = 0; columns < x; columns++) {
-//             $( '#container' ).prepend( "<div class='dot'></div>" );
-//         };
-//     };
-//     $( '.dot' ).width(200/x);
-//     $( '.dot' ).height(200/x);
-// }
-
-// $(function() {
-// 	createDots(24);
-// })
-
-
-
-
-
-
-
 // FUNCTION TO TRANSLATE 'R'
 
 function startTrans() {
 	var letter = $('#letter');
 	letter.animate({
-	left: 168,
-	top: 162
+	left: 164,
+	top: 156
 	}, 3000, 'swing', function() {
 		resetTrans();
 	});
@@ -44,8 +14,8 @@ function startTrans() {
 function resetTrans() {
 	var letter = $('#letter');
 	letter.animate({
-	left: -5,
-	top: 12
+	left: 2,
+	top: 16
 	}, 3000, 'swing', function() {
 		startTrans()
 	});
@@ -55,17 +25,6 @@ function resetTrans() {
 $(function() {
 	startTrans();
 });
-
-
-
-
-	
-
-
-
-
-
-
 
 
 // FUNCTION FOR STICKY DOTS
